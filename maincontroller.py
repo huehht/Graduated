e4a1d0ca9678cca3a1953495c52bfb30719bdfb3
huehht <1420356569@qq.com>
2023-04-20 17:59:12 +0800

    MLS-MPM-half

--- maincontroller.py
+++ maincontroller.py
@@ -49,28 +49,114 @@ class Simulation:
         E, nu = 5e3, 0.2  # Young's modulus and Poisson's ratio
         self.mu_0, self.lambda_0 = E / (2 * (1 + nu)), E * nu / (
             (1 + nu) * (1 - 2 * nu))  # Lame parameters
+        self.grid_v = ti.Vector.field(
+            2, dtype=float,
+            shape=(self.n_grid, self.n_grid))  # grid node momentum/velocity
+        self.grid_m = ti.field(dtype=float,
+                               shape=(self.n_grid,
+                                      self.n_grid))  # grid node mass
+        self.particles = []
 
     class particle:
 
-        def __init__(self) -> None:
+        def __init__(self,
+                     x,
+                     v=[0, 0],
+                     C=ti.Matrix.zero(float, 2, 2),
+                     F=ti.Matrix([[1, 0], [0, 1]]),
+                     material=MatterType.Solid,
+                     Jp=1,
+                     colour=Qt.red) -> None:
 
-            x = ti.Vector.field(2, dtype=float, shape=n_particles)  # position
-            v = ti.Vector.field(2, dtype=float, shape=n_particles)  # velocity
-            C = ti.Matrix.field(2, 2, dtype=float,
-                                shape=n_particles)  # affine velocity field
-            F = ti.Matrix.field(2, 2, dtype=float,
-                                shape=n_particles)  # deformation gradient
-            material = ti.field(dtype=int, shape=n_particles)  # material id
-            Jp = ti.field(dtype=float,
-                          shape=n_particles)  # plastic deformation
-            grid_v = ti.Vector.field(
-                2, dtype=float,
-                shape=(n_grid, n_grid))  # grid node momentum/velocity
-            grid_m = ti.field(dtype=float,
-                              shape=(n_grid, n_grid))  # grid node mass
-            gravity = ti.Vector.field(2, dtype=float, shape=())
-            attractor_strength = ti.field(dtype=float, shape=())
-            attractor_pos = ti.Vector.field(2, dtype=float, shape=())
+            self.x = x  # position
+            self.v = v  # velocity
+            self.C = C  # affine velocity field
+            self.F = F  # deformation gradient
+            self.material = material  # material id
+            self.Jp = Jp  # plastic deformation
+            self.colour = colour
+
+    @ti.kernel
+    def substep(self):
+        for i, j in self.grid_m:
+            self.grid_v[i, j] = [0, 0]
+            self.grid_m[i, j] = 0
+        for p in self.particles:  # Particle state update and scatter to grid (P2G)
+            base = (p.x * inv_dx - 0.5).cast(int)
+            fx = p.x * inv_dx - base.cast(float)
+            # Quadratic kernels  [http://mpm.graphics   Eqn. 123, with x=fx, fx-1,fx-2]
+            w = [0.5 * (1.5 - fx)**2, 0.75 - (fx - 1)**2, 0.5 * (fx - 0.5)**2]
+            # deformation gradient update
+            p.F = (ti.Matrix.identity(float, 2) + dt * p.C) @ p.F
+            # Hardening coefficient: snow gets harder when compressed
+            h = ti.max(0.1, ti.min(5, ti.exp(10 * (1.0 - p.Jp))))
+            if material[p] == 1:  # jelly, make it softer
+                h = 0.3
+            mu, la = self.mu_0 * h, self.lambda_0 * h
+            if material[p] == 0:  # liquid
+                mu = 0.0
+            U, sig, V = ti.svd(p.F)
+            J = 1.0
+            for d in ti.static(range(2)):
+                new_sig = sig[d, d]
+                if material[p] == 2:  # Snow
+                    new_sig = min(max(sig[d, d], 1 - 2.5e-2),
+                                  1 + 4.5e-3)  # Plasticity
+                p.Jp *= sig[d, d] / new_sig
+                sig[d, d] = new_sig
+                J *= new_sig
+            if material[p] == 0:
+                # Reset deformation gradient to avoid numerical instability
+                p.F = ti.Matrix.identity(float, 2) * ti.sqrt(J)
+            elif material[p] == 2:
+                # Reconstruct elastic deformation gradient after plasticity
+                p.F = U @ sig @ V.transpose()
+            stress = 2 * mu * (p.F - U @ V.transpose()) @ p.F.transpose(
+            ) + ti.Matrix.identity(float, 2) * la * J * (J - 1)
+            stress = (-dt * self.p_vol * 4 * inv_dx * inv_dx) * stress
+            affine = stress + self.p_mass * p.C
+            for i, j in ti.static(ti.ndrange(3, 3)):
+                # Loop over 3x3 grid node neighborhood
+                offset = ti.Vector([i, j])
+                dpos = (offset.cast(float) - fx) * dx
+                weight = w[i][0] * w[j][1]
+                self.grid_v[base +
+                            offset] += self.weight * (self.p_mass * v[p] +
+                                                      affine @ dpos)
+                self.grid_m[base + offset] += weight * self.p_mass
+        for i, j in grid_m:
+            if grid_m[i, j] > 0:  # No need for epsilon here
+                # Momentum to velocity
+                grid_v[i, j] = (1 / grid_m[i, j]) * grid_v[i, j]
+                grid_v[i, j] += dt * gravity[None] * 30  # gravity
+                dist = attractor_pos[None] - dx * ti.Vector([i, j])
+                grid_v[i, j] += \
+                    dist / (0.01 + dist.norm()) * attractor_strength[None] * dt * 100
+                if i < 3 and grid_v[i, j][0] < 0:
+                    grid_v[i, j][0] = 0  # Boundary conditions
+                if i > n_grid - 3 and grid_v[i, j][0] > 0:
+                    grid_v[i, j][0] = 0
+                if j < 3 and grid_v[i, j][1] < 0:
+                    grid_v[i, j][1] = 0
+                if j > n_grid - 3 and grid_v[i, j][1] > 0:
+                    grid_v[i, j][1] = 0
+        for p in x:  # grid to particle (G2P)
+            base = (x[p] * inv_dx - 0.5).cast(int)
+            fx = x[p] * inv_dx - base.cast(float)
+            w = [
+                0.5 * (1.5 - fx)**2, 0.75 - (fx - 1.0)**2, 0.5 * (fx - 0.5)**2
+            ]
+            new_v = ti.Vector.zero(float, 2)
+            new_C = ti.Matrix.zero(float, 2, 2)
+            for i, j in ti.static(ti.ndrange(3, 3)):
+                # loop over 3x3 grid node neighborhood
+                dpos = ti.Vector([i, j]).cast(float) - fx
+                g_v = grid_v[base + ti.Vector([i, j])]
+                weight = w[i][0] * w[j][1]
+                new_v += weight * g_v
+                new_C += 4 * inv_dx * weight * g_v.outer_product(dpos)
+            v[p], C[p] = new_v, new_C
+            x[p] += dt * v[p]  # advection
 
 
 class Minidraw_controller(QWidget):
